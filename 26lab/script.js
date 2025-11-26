17a73fd6d6f9c573568e8dabda28389b59b42eb2
Haskaam <kuba.stasinski@wp.pl>
2025-11-26 14:52:47 +0100

    Add files via upload

---
+++ 26lab/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typed = new Typed("#typed-word", {
+    strings: ["nowoczesne", "innowacyjne", "funkcjonalne", "skuteczne"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true,
+    smartBackspace: true
+  });
+});
